50439bd299f5f50294f5e01b1ab385ae3d9fbe70
Ian Rios <ian.rios1a@gmail.com>
2023-10-11 14:01:30 -0400

    added firebase

---
+++ src/app/data.js
@@ -0,0 +1,61 @@
+export const contactData = {
+    email: 'crumbculturelex@gmail.com',
+    website: 'crumbculture.com'
+}
+
+export const links = [
+    {
+        name: 'TikTok',
+        username: '',
+        url: 'https://www.tiktok.com/@crumb_culture',
+        icon: '',
+    },
+    {
+        name: 'Twitter / X',
+        username: '',
+        url: 'https://twitter.com/crumb_culture',
+        icon: '',
+    },
+    {
+        name: 'Instagram',
+        username: '',
+        url: 'https://www.instagram.com/crumb_culture_lex/',
+        icon: '',
+    },
+    {
+        name: 'Twitch',
+        username: '',
+        url: 'https://www.twitch.tv/crumb_culture_lex',
+        icon: '',
+    },
+    {
+        name: 'LinkedIn',
+        username: '',
+        url: 'https://www.linkedin.com/company/crumb-culture',
+        icon: '',
+    },
+    {
+        name: 'YouTube',
+        username: '',
+        url: 'https://www.youtube.com/@CrumbCulture',
+        icon: '',
+    },
+    {
+        name: 'Facebook',
+        username: '',
+        url: 'https://facebook.com/crumb.culture.lex',
+        icon: '',
+    },
+    {
+        name: 'Discord',
+        username: '',
+        url: 'https://discord.gg/NGkx8JvyVC',
+        icon: '',
+    },
+    {
+        name: 'Pinterest',
+        username: '',
+        url: 'https://www.pinterest.com/crumbculture/',
+        icon: '',
+    }
+]
---
+++ todo.md
@@ -0,0 +1,11 @@
+# todos
+
+- [ ] add links
+- [ ] add styles
+- [ ] add data scraping notice
+- [ ] add firebase
+- [ ] add info about site and typical site pages for info
+- [ ] remove netlify
+- [ ] turn off netlify ci/cd
+- [ ]
+- [ ]
